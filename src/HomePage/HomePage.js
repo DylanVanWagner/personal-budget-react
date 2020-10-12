db01afb4a396fb40029f90f3c269f63fa79d471a
Dylan Van Wagner <dvanwagn@uncc.edu>
2020-10-12 00:13:20 -0400

    Exercise 03

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,78 +1,77 @@
 import React from 'react';
+import Chart from '../Chart/Chart';
 
 function HomePage() {
   return (
-    <main className="center" id="main">
+    <div className="container center">
+      <main className="page-area" id="main">
+        <section className="text-box">
+          <h2>Stay on track</h2>
+          <p>
+            Do you know where you are spending your money? If you really stop to
+            track it down, you would get surprised! Proper budget management
+            depends on real data... and this app will help you with that!
+          </p>
+        </section>
 
-    <div className="page-area">
+        <section className="text-box">
+          <h2>Alerts</h2>
+          <p>
+            What if your clothing budget ended? You will get an alert. The goal
+            is to never go over the budget.
+          </p>
+        </section>
 
-        <article>
-            <h1>Stay on track</h1>
-            <p>
-                Do you know where you are spending your money? If you really stop to track it down,
-                you would get surprised! Proper budget management depends on real data... and this
-                app will help you with that!
-            </p>
-        </article>
+        <section className="text-box">
+          <h2>Results</h2>
+          <p>
+            People who stick to a financial plan, budgeting every expense, get
+            out of debt faster! Also, they live happier lives... since they
+            expend without guilt or fear... because they know it is all good and
+            accounted for.
+          </p>
+        </section>
 
-        <article>
-            <h1>Alerts</h1>
-            <p>
-                What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
-            </p>
-        </article>
+        <section className="text-box">
+          <h2>Free</h2>
+          <p>This app is free!!! And you are the only one holding your data!</p>
+        </section>
 
-        <article>
-            <h1>Results</h1>
-            <p>
-                People who stick to a financial plan, budgeting every expense, get out of debt faster!
-                Also, they to live happier lives... since they expend without guilt or fear... 
-                because they know it is all good and accounted for.
-            </p>
-        </article>
+        <section className="text-box">
+          <h2>Stay on track</h2>
+          <p>
+            Do you know where you are spending your money? If you really stop to
+            track it down, you would get surprised! Proper budget management
+            depends on real data... and this app will help you with that!
+          </p>
+        </section>
 
-        <article>
-            <h1>Free</h1>
-            <p>
-                This app is free!!! And you are the only one holding your data!
-            </p>
-        </article>
+        <section className="text-box">
+          <h2>Alerts</h2>
+          <p>
+            What if your clothing budget ended? You will get an alert. The goal
+            is to never go over the budget.
+          </p>
+        </section>
 
-        <article>
-            <h1>Stay on track</h1>
-            <p>
-                Do you know where you are spending your money? If you really stop to track it down,
-                you would get surprised! Proper budget management depends on real data... and this
-                app will help you with that!
-            </p>
-        </article>
+        <section className="text-box">
+          <h2>Results</h2>
+          <p>
+            People who stick to a financial plan, budgeting every expense, get
+            out of debt faster! Also, they to live happier lives... since they
+            expend without guilt or fear... because they know it is all good and
+            accounted for.
+          </p>
+        </section>
 
-        <article>
-            <h1>Alerts</h1>
-            <p>
-                What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
-            </p>
-        </article>
-
-        <article>
-            <h1>Results</h1>
-            <p>
-                People who stick to a financial plan, budgeting every expense, get out of debt faster!
-                Also, they to live happier lives... since they expend without guilt or fear... 
-                because they know it is all good and accounted for.
-            </p>
-        </article>
-
-        <article>
-            <h1>Chart</h1>
-            <p>
-                <canvas id="myChart" width="400" height="400"></canvas>
-            </p>
-        </article>
-
+        <section className="text-box">
+          <h2>Chart</h2>
+          <p>
+            <Chart/>
+          </p>
+        </section>
+      </main>
     </div>
-
-    </main>
   );
 }
 
